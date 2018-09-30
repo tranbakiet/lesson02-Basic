1e218353b383b2b6391c33846aa3e2d5b3e968d7
Kiet <206love193@gmail.com>
2018-09-30 23:07:54 +0700

    Finish for loop

--- src/kiet/dev/Basic.java
+++ src/kiet/dev/Basic.java
@@ -1,28 +1,12 @@
 package kiet.dev;
 
-import java.util.Scanner;
-
-public class Basic {
+public class Basic {	
 	
-	public static void main(String args[]){
-	      
-		  char n = '5';
-	      switch(n)
-	      {
-	         case '0' : System.out.println("zero!"); break;
-	         case '1' : System.out.println("one!"); break;
-	         case '2' : System.out.println("two!"); break;
-	         case '3' : System.out.println("three!"); break;
-	         case '4' : System.out.println("four!"); break;
-	         case '5' : System.out.println("five!"); break;
-	         case '6' : System.out.println("six!"); break;
-	         case '7' : System.out.println("seven!"); break;
-	         case '8' : System.out.println("eight!"); break;
-	         case '9' : System.out.println("nine!"); break;
-
-	         default :
-	            System.out.println("khong co");
-	      }
-	      System.out.println("Tu tieng anh nam o vi tri " + n);
-	 }
+	public static void main(String[] args) {
+        int a = 0;
+        for (int i = 1; i <= 100; i++) { 
+            a = a + i;
+        }
+        System.out.println("TONG = " + a);
+    }
 }
