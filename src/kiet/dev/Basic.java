9d095346c3a4ef73530a6b49f53be6d005f11bc5
Kiet <206love193@gmail.com>
2018-09-30 22:48:52 +0700

    Finish if conditional

--- src/kiet/dev/Basic.java
+++ src/kiet/dev/Basic.java
@@ -4,25 +4,49 @@ import java.util.Scanner;
 
 public class Basic {
 
-	public static void main(String[] args) {
-		int a, b;
-		Scanner sc = new Scanner(System.in);
-        System.out.print("a = ");
-        a = sc.nextInt();
-        System.out.print("b = ");
-        b = sc.nextInt();
-  
-        int tong = a + b;
-	    System.out.println(a + " + " + b + " = " + tong); 
-	    
-	    int hieu = a - b;
-	    System.out.println(a + " - " + b + " = " + hieu);  
-	    
-	    int tich = a * b ;
-	    System.out.println(a + " * " + b + " = " + tich);	
-	    
-	    int thuong = a / b ;
-	    System.out.println(a + " / " + b + " = " + thuong);
+	public static void main(String[] args) {	
+	   int a,b,c ;  
+	   a =Nhapsonguyen("Nhap he so a : ");
+	   b =Nhapsonguyen("Nhap he so b : ");
+	   c =Nhapsonguyen("Nhap he so c : ");
+	   GPTB2 (a,b,c);
 	}
-
+	
+	public static int Nhapsonguyen(String str) {
+		int x;
+		Scanner sc=new Scanner (System.in);
+        System.out.println(str);
+        x=sc.nextInt();
+        return x;
+	}
+	
+	public static void GPTB2 (int a, int b, int c) { 
+		if(a==0) {
+			if(b==0){
+				if(c==0)
+					System.out.println("Phuong trinh vo so nghiem");
+					else
+					System.out.println("Phuong trinh vo nghiem");
+			        }
+					else 
+				    System.out.println("PT co 1 nghiem duy nhat la x ="+(-c/b));
+		        }
+					else
+		        {
+					double x1;
+					double x2;
+					double delta=Math.pow(b, 2)-4*a*c;
+					if (delta<0)
+				    System.out.println("Phuonng trinh vo nghiem");
+					else if (delta>0)
+			    {
+					x1=(-b+Math.sqrt(delta)/(2*a));
+					x2=(-b-Math.sqrt(delta)/(2*a)); 
+	                System.out.println("x1= "+x1); 
+	                System.out.println("x2= "+x2); 	
+	            } 
+	            else
+	                System.out.println("phuong trinh co nghiem kep x1=x2="+(-b/2*a)); 
+	           } 
+        }
 }
